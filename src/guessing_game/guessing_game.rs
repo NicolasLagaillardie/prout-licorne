e804ae622ec02e34323d891b1370a98a387b5771
Nicolas LAGAILLARDIE <43790034+NicolasLagaillardie@users.noreply.github.com>
2026-01-17 01:52:48 +0100

    feat:added shadowing

--- src/guessing_game/guessing_game.rs
+++ src/guessing_game/guessing_game.rs
@@ -2,12 +2,33 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+const CONST_SEED: u32 = 30;
+
+fn shadowing() {
+    let mut x = 5;
+
+    println!("The first value of x is: {x}");
+
+    x += 5;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The second value of x is: {x}");
+}
+
 pub(crate) fn main() {
+    shadowing();
+
     println!("Guess the number!");
 
     let secrect_number = rand::rng().random_range(1..=100);
 
     println!("The secret number: {secrect_number}");
+
+    println!("The const number: {CONST_SEED}");
 
     loop {
         println!("Please input your guess.");
